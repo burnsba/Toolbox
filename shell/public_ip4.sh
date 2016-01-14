ef5cb96de9d63251a879abcea97494052ccbc1b7
Ben Burns <benjaminaburns@gmail.com>
2016-01-13 18:43:18 -0600

    Update public_ip4.sh

--- shell/public_ip4.sh
+++ shell/public_ip4.sh
@@ -3,9 +3,26 @@
 if [[ -z "${IFC_OUT}" ]]; then
     IFC_OUT=$(/sbin/ifconfig)
 fi
-echo "$IFC_OUT" | grep "^eth\|^venet" | awk '{print $1}' | \
-    while read -r LINE ; do
-            echo "$IFC_OUT" | grep "$LINE" -A 1 | grep "inet addr" | \
-                    awk '{print $2}' | awk -F ':' '{print $2}' | \
-                    sed 's/^127.*$//g' | uniq -1 | sed ' /^$/d'
-    done | xargs
+
+INET_ADDR=$(echo "$IFC_OUT" | grep "inet addr" | wc -l)
+if [[ "${INET_ADDR}" > 0 ]]; then
+    USE_COLON=1
+else
+    USE_COLON=0
+fi
+
+if [[ "${USE_COLON}" == 1 ]]; then
+    echo "$IFC_OUT" | grep "^eth\|^venet\|^wlan" | awk '{print $1}' | \
+        while read -r LINE ; do
+                    echo "$IFC_OUT" | grep "$LINE" -A 1 | grep "inet addr" | \
+                            awk '{print $2}' | awk -F ':' '{print $2}' | \
+                            sed 's/^127.*$//g' | uniq -1 | sed ' /^$/d'
+        done | xargs
+else
+    echo "$IFC_OUT" | grep "^eth\|^venet\|^wlan" | awk '{print $1}' | \
+        while read -r LINE ; do
+                    echo "$IFC_OUT" | grep "$LINE" -A 1 | grep "inet " | \
+                            awk '{print $2}' | \
+                            sed 's/^127.*$//g' | uniq -1 | sed ' /^$/d'
+        done | xargs
+fi
